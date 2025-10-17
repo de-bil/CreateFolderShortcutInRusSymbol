b7b279e97d9449fb8db2517de69c81d4c6957fc5
de-bil <filimonovmaxick@gmail.com>
2025-10-17 10:03:48 +0300

    CreateFolder Shortcut.bat

--- CreateFolder Shortcut.bat
+++ CreateFolder Shortcut.bat
@@ -1,4 +1,5 @@
 @echo off
+REM Поддержка кириллице в консоли
 chcp 65001 >nul
 
 echo Проверка и создание ярлыка через PowerShell...
@@ -8,5 +9,6 @@ set "FULL_PATH=*сюда вставить uml путь до конкретной
 
 REM Выполняем проверку и создание ярлыка через PowerShell
 powershell -ExecutionPolicy Bypass -Command "$fullPath='%FULL_PATH%'; $desktopPath=[Environment]::GetFolderPath('Desktop'); $shortcutFullPath=Join-Path $desktopPath '*Имя ярлыка на кириллице в том числе*.lnk'; if(Test-Path $shortcutFullPath) { $shortcut=(New-Object -ComObject WScript.Shell).CreateShortcut($shortcutFullPath); if(($shortcut.TargetPath.Split('\\')[-1].Trim().ToLower() -eq 'explorer.exe') -and ($shortcut.Arguments.Trim() -eq $fullPath.Trim())) { Write-Host 'Ярлык уже настроен верно, замена не требуется.'; Exit } else { Write-Host 'Обнаружено несоответствие целей ярлыков. Удаляю существующий ярлык и создаю новый...'; Remove-Item $shortcutFullPath -Force } } else { Write-Host 'Ярлык не найден, создаём новый...' }; $WshShell=New-Object -ComObject WScript.Shell; $newShortcut=$WshShell.CreateShortcut($shortcutFullPath); $newShortcut.TargetPath='explorer.exe'; $newShortcut.Arguments=$fullPath; $newShortcut.WorkingDirectory='C:\\'; $newShortcut.Description='Сетевая папка'; $newShortcut.IconLocation='shell32.dll,4'; $newShortcut.Save(); Write-Host 'Ярлык успешно создан или обновлён!'"
+
 
 echo.
